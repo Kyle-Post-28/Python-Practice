5bf7ca3cdbc21f9c638c1b5a37df5af073c307d1
Kyle Post <thekylepost@gmail.com>
2017-05-18 09:49:11 -0400

    Add files via upload

---
+++ exercise34.py
@@ -0,0 +1,40 @@
+#Filename: exercise 34.py
+#Author: Kyle Post
+#Date: May 17, 2017
+#Purpose: To practice lists and the elemnets
+#in them. Learn the difference between ordinal
+#and cardinal numbers
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+#Find the animal at 1. (Cardinal) 
+#Answer guess: python
+python = animal[1]
+
+#Find the third animal. (Ordinal)
+#Answer guess: peacock
+peacock = animal[2]
+
+#Find the first animal. (Ordinal)
+#Answer guess: bear
+bear = animal[0]
+
+#Find the animal at 3. (Cardinal)
+#Answer guess: kangaroo
+kangaroo = animal[3]
+
+#Find the fifth animal. (Ordinal)
+#Answer guess: whale
+whale = animal[4]
+
+#Find the animal at 2. (Cardinal)
+#Answer guess: peacock
+peacock = animal[2]
+
+#Find the 6th animal. (Ordinal)
+#Answer guess: platypus
+platypus = animal[5]
+
+#Find the animal at 4. (Cardinal)
+#Answer guess: whale
+whale = animal[4]
